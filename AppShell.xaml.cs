099fa7b239f8008279ec6e307b852fc28f9374f3
Sheng hao Dong <gungnirstan@gmail.com>
2021-11-17 14:30:12 -0500

    preview file

---
+++ AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FreshBox
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Current.GoToAsync("//LoginPage");
+        }
+
+        //send user to Home page on click from bottom navbar
+        private async void HomePage(object sender, EventArgs e)
+        {
+            await Current.GoToAsync("//AboutPage");
+        }
+        
+    }
+}
